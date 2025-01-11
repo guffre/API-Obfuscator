297c5075053a3b360ce550dc94962ebae9e1724d
guffre <mtguffr@gmail.com>
2025-01-11 01:18:23 -0600

    Added macro generator for new functions; Improved sample.c

--- apiresolver.h
+++ apiresolver.h
@@ -6,22 +6,26 @@
 #include <time.h>
 #include <stdio.h>
 
+// For LookupPrivilegeValueA
+#pragma comment(lib, "advapi32.lib")
+
 // Function declarations
 void  InitApiResolver(void);
 PVOID ApiResolver(unsigned int API);
 
 // Some global values
-#define APILENGTH 4                     // How many random values point to the real function. See apiresolverstruct for how its used.
+#define APILENGTH 4                     // How many random values point to the real function. Really this is just "jitter" to add math
 unsigned int ApiResolverCounter = 0;    // This is used to not get array accesses optimized away into a single value.
 unsigned int ApiResolverSeed = 0;       // We are using rand() deterministically, so save the seed(). Not for crypto, its fine to use time()
-PVOID ApiResolverAPIs[] = { &VirtualProtect, &VirtualAlloc, &LoadLibraryA, &GetProcAddress };
 
-typedef LPVOID  (WINAPI * VIRTUALALLOC)( LPVOID, SIZE_T, DWORD, DWORD );
-typedef BOOL  (WINAPI * VIRTUALPROTECT)( LPVOID, SIZE_T, DWORD, PDWORD );
-typedef HMODULE (WINAPI * LOADLIBRARYA)( LPCSTR );
-typedef FARPROC (WINAPI * GETPROCADDRESS)( HMODULE, LPCSTR );
-typedef BOOL (WINAPI * LOOKUPPRIVILEGEVALUEA)( LPCSTR, LPCSTR, PLUID );
+typedef LPVOID  (WINAPI * VirtualAlloc_T)( LPVOID, SIZE_T, DWORD, DWORD );
+typedef BOOL  (WINAPI * VirtualProtect_T)( LPVOID, SIZE_T, DWORD, PDWORD );
+typedef HMODULE (WINAPI * LoadLibraryA_T)( LPCSTR );
+typedef FARPROC (WINAPI * GetProcAddress_T)( HMODULE, LPCSTR );
+typedef BOOL (WINAPI * LookupPrivilegeValueA_T)( LPCSTR, LPCSTR, PLUID );
 
+// These must be in the same order. The enum is just to give useful names for the API_DEFINE macro
+PVOID ApiResolverAPIs[] = { &VirtualProtect, &VirtualAlloc, &LoadLibraryA, &GetProcAddress, &LookupPrivilegeValueA };
 enum ApiOffsetOrder {
     ApiVirtualProtect,
     ApiVirtualAlloc,
@@ -33,30 +37,37 @@ enum ApiOffsetOrder {
 
 UINT apiresolverstruct[APILENGTH*ApiEnumCount];
 
-#define VirtualProtect ((VIRTUALPROTECT)ApiResolver(((UINT*)&apiresolverstruct)[APILENGTH*ApiVirtualProtect + (ApiResolverCounter++%APILENGTH)]))
-#define VirtualAlloc     ((VIRTUALALLOC)ApiResolver(((UINT*)&apiresolverstruct)[APILENGTH*ApiVirtualAlloc   + (ApiResolverCounter++%APILENGTH)]))
-#define LoadLibraryA     ((LOADLIBRARYA)ApiResolver(((UINT*)&apiresolverstruct)[APILENGTH*ApiLoadLibraryA   + (ApiResolverCounter++%APILENGTH)]))
-#define GetProcAddress ((GETPROCADDRESS)ApiResolver(((UINT*)&apiresolverstruct)[APILENGTH*ApiGetProcAddress + (ApiResolverCounter++%APILENGTH)]))
-#define LookupPrivilegeValueA ((LOOKUPPRIVILEGEVALUEA)ApiResolver(((UINT*)&apiresolverstruct)[APILENGTH*ApiLookupPrivilegeValueA + (ApiResolverCounter++%APILENGTH)]))
+// Name##_T -> VirtualProtect_T ; Api##Name -> ApiVirtualProtect
+#define API_DEFINE(Name) ((Name##_T)ApiResolver(((UINT*)&apiresolverstruct)[APILENGTH*Api##Name + (ApiResolverCounter++ % APILENGTH)]))
 
-// Function definitions if using as single header
+#define VirtualProtect API_DEFINE(VirtualProtect)
+#define VirtualAlloc API_DEFINE(VirtualAlloc)
+#define LoadLibraryA API_DEFINE(LoadLibraryA)
+#define GetProcAddress API_DEFINE(GetProcAddress)
+#define LookupPrivilegeValueA API_DEFINE(LookupPrivilegeValueA)
+
+// Initializes the apiresolverstruct. This becomes a map of value ->      offset       -> function address.
+//                                                           API  -> apiresolverstruct ->  ApiResolverAPIs
 void InitApiResolver(void)
 {
     ApiResolverSeed = time(NULL);
     srand(ApiResolverSeed);
 
+    // This delta is very important. It randomizes the array order in memory to frustrate dynamic analysis.
+    // For example, LoadLibraryA can be found at apiresolverstruct[0] depending on this delta.
     unsigned int delta  = rand()%(sizeof(apiresolverstruct)/sizeof(UINT));
     size_t real_size    = sizeof(apiresolverstruct)/sizeof(UINT);
     unsigned int offset = rand()%512;
     
     for (size_t i = 0; i < real_size; i++)
     {
-        // printf("[%zu] addr: %p = %u (offset)\n", i, &((UINT*)&apiresolverstruct)[(i+delta)%real_size], offset);
+        // printf("[%zu + %u] addr: %p = %u (offset)\n", i, delta, &((UINT*)&apiresolverstruct)[(i+delta)%real_size], offset);
         ((UINT*)&apiresolverstruct)[(i+delta)%real_size] = offset;
         offset += rand()%512;
     }
 }
 
+// This turns the value API back into an offset into apiresolverstruct, which gets the actual function address
 PVOID ApiResolver(unsigned int API)
 {
     srand(ApiResolverSeed);
--- sample.c
+++ sample.c
@@ -1,9 +1,19 @@
+// cl.exe /O2 sample.c
 #include "apiresolver.h"
+
+#ifndef _APIRESOLVER_H
+#include <Windows.h>
+#include <stdio.h>
+#endif
+
+typedef int (*printf_t)(const char*, ...);
 
 // Sample Usage:
 int main(void)
 {
+    #ifdef _APIRESOLVER_H
     InitApiResolver();
+    #endif
 
     HANDLE advapi = LoadLibraryA("advapi32.dll");
     advapi = LoadLibraryA("advapi32.dll");
@@ -12,11 +22,18 @@ int main(void)
     advapi = LoadLibraryA("advapi32.dll");
 
     PVOID func = GetProcAddress(advapi, "AdjustTokenPrivileges");
-    func = GetProcAddress(advapi, "AdjustTokenPrivileges");
-    func = GetProcAddress(advapi, "AdjustTokenPrivileges");
-    func = GetProcAddress(advapi, "AdjustTokenPrivileges");
-    func = GetProcAddress(advapi, "AdjustTokenPrivileges");
-
     printf("advapi32: %p AdjustTokenPrivileges: %p\n", advapi, func);
 
+    HANDLE hModule = LoadLibraryA("msvcrt.dll");
+
+    printf_t print = (printf_t)GetProcAddress(hModule, "printf");
+    printf("msvcrt: %p printf: %p print: %p\n", hModule, printf, print);
+
+    if (print == NULL) {
+        printf("Failed to get printf addr.\n");
+        return 1;
+    }
+    
+    print("[+] This is the printf function loaded from LoadLibrary and GetProcAddress!\n");
+    print("[+] Everything worked!\n");
 }
